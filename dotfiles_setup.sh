fa5aa8ead02214943f5dac83147458b235c297e7
Sascha Roloff <sascha.roloff@gmail.com>
2019-09-10 14:07:53 +0200

    Added setup script to safely install dotfiles

---
+++ dotfiles_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TODO: script should only be executed from this directory
+# ${BASH_SOURCE[0]}
+
+dotfiles=(".vimrc" ".gitconfig" ".bashrc" ".profile" ".inputrc")
+for dotfile in "${dotfiles[@]}"; do
+    if [ -f "$HOME/$dotfile" ]; then
+        read -p "$HOME/$dotfile exists, override? (y/n): " res
+        if [ -n "$res" -a \( "$res" = "y" -o "$res" = "Y" \) ]; then
+            ln -sf "$PWD/$dotfile" "$HOME/$dotfile"
+            fi
+    else
+        ln -sf "$PWD/$dotfile" "$HOME/$dotfile"
+    fi
+done
