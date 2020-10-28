a0279dc60c18206a043fec4fbebf699bb7368571
Sascha Roloff <sascha.roloff@gmail.com>
2020-10-28 12:04:37 +0100

    Much better dotfiles setup

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # TODO: script should only be executed from this directory
 # ${BASH_SOURCE[0]}
 
 dotfiles=(~/.bashrc ~/.profile ~/.inputrc ~/.vimrc ~/.gitconfig ~/.tmux.conf ~/.config/Code/User/settings.json)
-for dotfile in "${dotfiles[@]}"; do
-    if [ -f "$dotfile" ]; then
-        read -p "$dotfile exists, override? (y/n): " res
-        if [ -n "$res" -a \( "$res" = "y" -o "$res" = "Y" \) ]; then
-            ln -sf "$(realpath $(basename $dotfile))" "$dotfile"
+for dotfile in ${dotfiles[@]}; do
+    _dotfile=$(realpath $(basename $dotfile))
+    if [ -f $dotfile ]; then
+        if [ $(realpath $dotfile) = $_dotfile ]; then
+            echo "$dotfile already linked"
+        else
+            read -p "$dotfile exists, override? (y/n): " res
+            if [ -n $res -a \( "$res" = "y" -o "$res" = "Y" \) ]; then
+                ln -sf $_dotfile $dotfile
+            fi
         fi
     else
-        ln -sf "$(realpath $(basename $dotfile))" "$dotfile"
+        echo "$dotfile does not exist, link it"
+        mkdir -p $(dirname $dotfile)
+        ln -sf $_dotfile $dotfile
     fi
 done
 
