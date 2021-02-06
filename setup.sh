cbec7e037d070074d4beef0463e04cec5bca65a1
Roloff Sascha (XC-AD/ESB3) <sascha.roloff@gmail.com>
2021-02-06 14:19:07 +0100

    Added XDG user directory file, only Desktop and Downloads folder, the others can be removed

--- setup.sh
+++ setup.sh
@@ -3,9 +3,13 @@
 # TODO: script should only be executed from this directory
 # ${BASH_SOURCE[0]}
 
-dotfiles=(~/.bashrc ~/.profile ~/.inputrc ~/.vimrc ~/.gitconfig ~/.tmux.conf ~/.config/Code/User/settings.json)
+# list of target dotfiles (paths to their target directories)
+dotfiles=(~/.bashrc ~/.profile ~/.inputrc ~/.vimrc ~/.gitconfig ~/.tmux.conf ~/.config/Code/User/settings.json ~/.config/user-dirs.dirs)
 for dotfile in ${dotfiles[@]}; do
+    # determine local dotfile (path to this directory)
     _dotfile=$(realpath $(basename $dotfile))
+
+    # check if target dotfile exists
     if [ -f $dotfile ]; then
         if [ $(realpath $dotfile) = $_dotfile ]; then
             echo "$dotfile already linked"
